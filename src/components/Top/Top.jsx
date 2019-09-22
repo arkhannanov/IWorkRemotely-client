a77c5a5141a2986fa3b79942b155ab44e4a0838a
arkhannanov <arkhannanov@gmail.com>
2019-09-22 18:17:19 +0300

    Edit I WORK REMOTELY HeadLine

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -9,9 +9,7 @@ const Top = () => {
       </div>
       <div className='top__content'>
         <div className='top__content-header'>i work remotely</div>
-        <div className='top__content-content'>I WORK REMOTELY - крупнейшее в мире сообщество удаленной работы.{'\n'}
-          Более 2,5 миллионов посетителей в месяц IWR является ресурсом № 1, где можно найти невероятные удаленные
-          вакансии.
+        <div className='top__content-content'>I WORK REMOTELY - сообщество достойных людей для достойных удаленных ваканский.
         </div>
         <NavLink to='/post-a-job' className='top__post-job-container'>
           <button className='top__post-job'>Разместить вакансию за 500 р.</button>
